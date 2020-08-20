7a28309552cfca8e84d392139a493658cebd162e
NicolasCavicchioli <nicknice@outlook.it>
2020-08-20 13:40:29 +0200

    Calendario, html+js

--- functions.js
+++ functions.js
@@ -1,1 +1,22 @@
 // Cavi was here
+function classFilter(){
+	const inputValue = document.getElementById("calendar_filter_input").value.toUpperCase();
+	var n = 0;
+	
+	if (!inputValue) return;
+	for (let element of document.getElementsByClassName("calendar_entry")){
+		element.classList.remove("active");
+		
+		element.getAttribute("data-class").split(" ").forEach(klass => {
+			if (klass == inputValue){
+				element.classList.add("active");
+				n++;
+			}
+		});
+		
+	}
+	
+	// counter
+	document.getElementById("calendar_filter_counter").innerHTML = (n == 0) ? "" : n + "found";
+	
+}
