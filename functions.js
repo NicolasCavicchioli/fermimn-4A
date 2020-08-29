027b59814870be6b09911849bab9012d9afe0832
NicolasCavicchioli <nicknice@outlook.it>
2020-08-29 12:50:32 +0200

    Title, 5th dropbtn, slides test, cards and more...

--- functions.js
+++ functions.js
@@ -1,15 +1,16 @@
-// Cavi was here
 function classFilter(){
 	const inputValue = document.getElementById("calendar_filter_input").value.toUpperCase();
 	const counter = document.getElementById("calendar_filter_counter");
 	var n = 0;
 	
 	if (inputValue) {
+		document.getElementsByClassName("calendar_wrapper")[0].classList.add('onlyActive');
 		
 		for (let element of document.getElementsByClassName("calendar_entry")){
 			element.classList.remove("active");
 			
-			element.getAttribute("data-class").split(" ").forEach(klass => {
+			element.getAttribute("data-class").split(" ")
+			.forEach(klass => {
 				if (klass == inputValue){
 					element.classList.add("active");
 					n++;
@@ -18,8 +19,14 @@ function classFilter(){
 			
 		}
 		
+	} else {
+		document.getElementsByClassName("calendar_wrapper")[0].classList.remove('onlyActive');
 	}
-	
-	counter.innerHTML = (n == 0 && !inputValue) ? "" : n + " found";
-	
+		
+	counter.innerHTML = (n == 0 && !inputValue) ? "" : n + " Found";
 }
+
+//if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+//	document.firstElementChild.setAttribute("data-theme", "dark");
+//	document.getElementById("theme_toggler").checked = true;
+//}
